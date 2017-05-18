e42acfcb347200584bccc64aaed5f7b41ba7c3f3
Tobias Kloht <mail@kloht.com>
2017-05-18 11:24:35 +0200

    use menu-select for zsh completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# activate menu selection
+zstyle ':completion:*' menu select
+
+zstyle ':completion:*' list-colors ''
